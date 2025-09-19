3a7591d07ee18cb08f20bf0a4497fd1027154706
pabloperguz <p.perez-guzman@imperial.ac.uk>
2025-09-19 09:48:06 +0100

    update run script

--- files/severity/script.R
+++ files/severity/script.R
@@ -7,9 +7,15 @@ model <- sir$new()
 t <- seq(0, 400, 1)
 
 y <- as.data.frame(model$run(t)) %>% 
-  mutate(step = step / 4) %>% 
-  pivot_longer(!step)
+  mutate(step = step / 4)
 
-ggplot(y, aes(step, value, col = name)) +
+y %>% 
+  pivot_longer(!step) %>% 
+  ggplot(aes(step, value, col = name)) +
   geom_line() +
   facet_wrap(~name, scales = "free_y")
+
+y %>% 
+  select(!step) %>% 
+  rowSums() %>% 
+  plot()
