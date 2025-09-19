c83251f28eea7760a116d104b2776d7790f61aca
Pablo Noel Perez Guzman <pperezgu@dide.local>
2025-09-19 14:00:35 +0100

    plot functions

--- files/severity/script.R
+++ files/severity/script.R
@@ -1,24 +1,29 @@
-
-pacman::p_load(rio, tidyverse, matrixStats, odin.dust, mcstate)
 source("files/severity/support.R")
+pacman::p_load(rio,            # import/export files
+               tidyverse,      # data cleaning, wrangling, plotting, etc. tools
+               patchwork,      # multi-panel plotting tools
+               matrixStats,    # stats on matrix object tools
+               odin.dust,      # stochastic state-space models DSL
+               mcstate)        # fitting tools for odin models
+
 
 sir <- odin.dust::odin_dust("files/severity/severity_model.R")
 
 pars <- list(
-  beta = 0.8,       # Transmission rate
+  beta = 0.38,      # Transmission rate
   p_sev = 0.3,      # Probability of severe disease if infected
   sigma_Is = 0.048, # Recovery rate from severe disease
   sigma_Ia = 0.141, # Recovery rate from mild disease
-  mu = 0.048,       # Mortality rate from severe disease
-  N = 10000,        # Total population
+  mu = 0.034,       # Mortality rate from severe disease
+  N = 1e6,          # Total population
   I_init = 10       # Initial infected
 )
 
-model <- sir$new(pars, time = 0, n_particles = 10)
-index <- model$info()$index
-t <- seq(0, 200, 1)
+y <- run_model(sir, pars, n_days = 40)
 
-y <- model$simulate(t)
+plot_timeseries(y, index)
 
-plot_timeseries(y, index, "hosp_incidence")
+plot_timeseries(y, index, "hosp_incidence") /
+  plot_timeseries(y, index, "deaths_incidence")
+
 
--- files/severity/support.R
+++ files/severity/support.R
@@ -1,4 +1,18 @@
 
+run_model <- function(mod, pars, n_days = 100, n_pars = 100) {
+  
+  n_steps <- n_days * 4
+  
+  model <- mod$new(pars, time = 0, n_particles = n_pars)
+  index <- model$info()$index
+  t <- seq(0, n_steps, 1)
+  
+  y <- model$simulate(t)
+  
+  y
+}
+
+
 summarise_state <- function(y) {
   data.frame(
     day = seq(1, nrow(y)) / 4,
@@ -13,30 +27,63 @@ plot_timeseries <- function(y, index, what = NULL) {
   
   stopifnot(what %in% names(index))
   
-  ylab <- if (what == "hosp_incidence") {
+  ylab <- if (is.null(what)) {
+    NULL
+  } else if (what == "hosp_incidence") {
     "Hospital admissions"
   } else if (what == "deaths_incidence") {
     "Daily deaths"
   }
 
-  tmp <- t(y[index[[what]], , ])
-  
-  ret <- summarise_state(tmp)
+  if (is.null(ylab)) {
     
-  ggplot(ret, aes(x = day)) +
-    geom_line(aes(y = mean, col = "Model")) +
-    geom_ribbon(aes(ymin = lb, ymax = ub, fill = "Model"), 
-                alpha = 0.2, show.legend = FALSE) +
-    labs(x = "Time", y = ylab) +
+    breaks <- c("Model hospitalisations", "Model Deaths", "Data")
+    values <- c("green3", "blue", "black")
+    
+    hosp <- t(y[index[["hosp_incidence"]], , ])
+    deaths <- t(y[index[["deaths_incidence"]], , ])
+    
+    hosp <- summarise_state(hosp) %>% 
+      mutate(type = "Model hospitalisations")
+    deaths <- summarise_state(deaths) %>% 
+      mutate(type = "Model Deaths")
+    
+    ret <- rbind(hosp, deaths)
+    
+    p <- ggplot(ret, aes(x = day)) +
+      geom_line(aes(y = mean, col = type)) +
+      geom_ribbon(aes(ymin = lb, ymax = ub, fill = type), 
+                  alpha = 0.2, show.legend = FALSE) +
+      labs(x = "Time", y = "Incidence") +
+      scale_color_manual(breaks = breaks, values = values) +
+      scale_fill_manual(breaks = breaks, values = values) +
+      annotate("text", label = "Outbreak detected", x = 24, y = 900)
+    
+    
+  } else {
+    
+    breaks <- c("Model", "Data")
+    values <- c("green3", "black")
+    
+    tmp <- t(y[index[[what]], , ])
+    ret <- summarise_state(tmp)
+    
+    p <- ggplot(ret, aes(x = day)) +
+      geom_line(aes(y = mean, col = "Model")) +
+      geom_ribbon(aes(ymin = lb, ymax = ub, fill = "Model"), 
+                  alpha = 0.2, show.legend = FALSE) +
+      labs(x = "Time", y = ylab) +
+      scale_color_manual(breaks = breaks, values = values) +
+      scale_fill_manual(breaks = c("Model", "Data"),
+                        values = c("green4", "black"))
+  }
+  
+  p +
+    geom_vline(xintercept = 30, linetype = 2, col = "red") +
     scale_x_continuous(expand = c(0, 0)) +
     scale_y_continuous(expand = c(0, 0)) +
-    scale_color_manual(breaks = c("Model", "Data"),
-                       values = c("green4", "red")) +
-    scale_fill_manual(breaks = c("Model", "Data"),
-                      values = c("green4", "red")) +
     theme_minimal() +
     theme(axis.line = element_line(),
-          legend.position = c(0.85, 0.85),
+          legend.position = c(0.25, 0.85),
           legend.title = element_blank())
-  
 }
