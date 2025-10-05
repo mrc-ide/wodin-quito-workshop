91ce52b41566af039acbae3c4ea7e71ec71c4cf8
ruthmccabe <ruth.mccabe17@imperial.ac.uk>
2025-10-05 12:20:16 -0500

    updated nomenclature (b = births; mu = deaths)

--- files/introduction/sir_open.R
+++ files/introduction/sir_open.R
@@ -1,7 +1,7 @@
 # variables
-deriv(S) <- Births - b * S - beta * S * I / N 
-deriv(I) <- beta * S * I / N - (b + sigma) * I
-deriv(R) <- sigma * I - b * R 
+deriv(S) <- b - mu * S - beta * S * I / N 
+deriv(I) <- beta * S * I / N - (mu + sigma) * I
+deriv(R) <- sigma * I - mu * R 
 
 # initial conditions of the variables
 initial(S) <- N - I0
@@ -13,8 +13,8 @@ output(pop) <- S + I + R
 # parameter values
 N <- 1e6                  # total population size
 I0 <- user(1)             # num infectious cases at start of epidemic
-beta <- R0 * (sigma + b)  # transmission parameter
+beta <- R0 * (sigma + mu)  # transmission parameter
 sigma <- user(2)          # recovery rate (1/mean duration infectiousness)
 R0 <- user(5)             # av num new cases caused by single infectious case
-b <- user(0.02)           # death rate (average life expectancy of 1 year or 52 weeks) = 1/52
-Births <- b * N           # number of births (for a constant population size)
+mu <- user(0.02)           # death rate (average life expectancy of 1 year or 52 weeks) = 1/52
+b <- mu * N           # number of births (for a constant population size)
